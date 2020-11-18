8a3e0ba05a851d71ac9c5230e96e8d27577154ff
Lukas Cerny <lukas.cerny@cookielab.io>
2020-11-19 00:27:25 +0100

    Remove unused packages

--- stow.sh
+++ stow.sh
@@ -4,9 +4,13 @@ sudo apt install
 sudo apt update
 sudo apt upgrade
 
-#stow compton
+stow compton
 stow konsole
 stow plasma
+#stow tmux
 stow X
+#stow bash
+#stow firefox
 stow i3
 stow sublime-text-3
+#stow vim
